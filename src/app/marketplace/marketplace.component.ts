e025eeb8da00bfe3ff7f8f59284fc7f778e9c85a
Elliot Burin and Jessie Waite <elliot@>
2017-12-11 10:08:18 -0800

    finsih online-store project

---
+++ src/app/marketplace/marketplace.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marketplace',
+  templateUrl: './marketplace.component.html',
+  styleUrls: ['./marketplace.component.css']
+})
+export class MarketplaceComponent {
+
+
+}
